0d1f0d7da66d87d462e2f675d0657b5d919f6ad8
Romeo509 <romeoameku503@gmail.com>
2024-06-27 16:04:11 +0000

    Interview passed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,39 +1,48 @@
 #!/usr/bin/python3
 """
-UTF-8 Validation script
+UTF-8 Validation
 """
-
-
 def validUTF8(data):
     """
-    Checks if the data represents a valid UTF-8 encoding.
-
-    Args:
-    data (list of int): The data to be checked.
-
-    Returns:
-    bool: True if data is valid UTF-8, else False.
+    Determine if a given dataset represents a valid UTF-8 encoding.
+    
+    :param data: List of integers representing 1 byte of data each
+    :return: True if data is a valid UTF-8 encoding, False otherwise
     """
-    num_bytes = 0
+    num_follows = 0
 
-    for byte in data:
-        if byte > 255:
-            return False
-
-        if num_bytes == 0:
-            if (byte >> 7) == 0b0:
-                num_bytes = 0
-            elif (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            else:
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+    
+    for num in data:
+        mask = 1 << 7
+        
+        """
+        Determine number of following bytes
+        """
+        if num_follows == 0:
+            while mask & num:
+                num_follows += 1
+                mask = mask >> 1
+            
+            """
+            Handle 1-byte characters (0xxxxxxx)
+            """
+            if num_follows == 0:
+                continue
+            
+            """
+            Handle multi-byte characters
+            """
+            if num_follows == 1 or num_follows > 4:
                 return False
         else:
-            if (byte >> 6) != 0b10:
+            """
+            Check if current byte is a continuation byte (10xxxxxx)
+            """
+            if not (num & mask1 and not (num & mask2)):
                 return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+            
+            num_follows -= 1
+    
+    return num_follows == 0
