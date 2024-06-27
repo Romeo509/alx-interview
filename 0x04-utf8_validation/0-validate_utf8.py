a52ca1f3c1e63d4267fd94509b1d7f06a71a3f4f
Romeo509 <romeoameku503@gmail.com>
2024-06-27 16:32:02 +0000

    Interview passed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,8 +4,8 @@
 
 def get_leading_set_bits(num):
     """Returns the number of leading set bits (1)"""
-    set_bits = 0
     helper = 1 << 7
+    set_bits = 0
     while helper & num:
         set_bits += 1
         helper = helper >> 1
@@ -17,37 +17,18 @@ def validUTF8(data):
     bits_count = 0
 
     for i in range(len(data)):
-        if not check_byte(data[i], bits_count):
-            return False
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(data[i])
+
+            if bits_count == 0:
+                continue
+
+            if bits_count == 1 or bits_count > 4:
+                return False
+        else:
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+                return False
 
         bits_count -= 1
 
     return bits_count == 0
-
-
-def check_byte(byte, bits_count):
-    """Checks if a byte is valid according to UTF-8 encoding rules"""
-    if bits_count == 0:
-        bits_count = get_leading_set_bits(byte)
-
-        if bits_count == 0:
-            return True
-
-        if bits_count == 1 or bits_count > 4:
-            return False
-    else:
-        if not (byte & (1 << 7) and not (byte & (1 << 6))):
-            return False
-
-    return True
-
-
-if __name__ == "__main__":
-    data_sets = [
-        [65],
-        [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33],
-        [229, 65, 127, 256]
-    ]
-
-    for data in data_sets:
-        print(validUTF8(data))
