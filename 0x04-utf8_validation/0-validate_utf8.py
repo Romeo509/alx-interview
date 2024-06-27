fa6bac7fa1a0efd02e6c938f8856372f07116ab8
Romeo509 <romeoameku503@gmail.com>
2024-06-27 16:27:45 +0000

    Interview passed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,33 +2,46 @@
 """UTF-8 Validation"""
 
 
-def get_leading_set_bits(num):
-    """Returns the number of leading set bits (1)"""
-    set_bits = 0
-    helper = 1 << 7
-    while helper & num:
-        set_bits += 1
-        helper = helper >> 1
-    return set_bits
+def count_leading_ones(byte):
+    """Count the number of leading 1 bits in the byte"""
+    mask = 1 << 7
+    count = 0
+    while byte & mask:
+        count += 1
+        mask >>= 1
+    return count
 
 
 def validUTF8(data):
-    """Determines if a given dataset represents a valid UTF-8 encoding"""
-    bits_count = 0
+    """Determine if the given dataset represents a valid UTF-8 encoding"""
+    bytes_to_check = 0
 
-    for i in range(len(data)):
-        if bits_count == 0:
-            bits_count = get_leading_set_bits(data[i])
-
-            if bits_count == 0:
-                continue
-
-            if bits_count == 1 or bits_count > 4:
+    for num in data:
+        if bytes_to_check == 0:
+            if (num >> 5) == 0b110:
+                bytes_to_check = 1
+            elif (num >> 4) == 0b1110:
+                bytes_to_check = 2
+            elif (num >> 3) == 0b11110:
+                bytes_to_check = 3
+            elif (num >> 7) == 0:
+                bytes_to_check = 0
+            else:
                 return False
         else:
-            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+            if (num >> 6) != 0b10:
                 return False
+            bytes_to_check -= 1
 
-        bits_count -= 1
+    return bytes_to_check == 0
 
-    return bits_count == 0
+
+if __name__ == "__main__":
+    data_sets = [
+        [65],
+        [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33],
+        [229, 65, 127, 256]
+    ]
+
+    for data in data_sets:
+        print(validUTF8(data))
